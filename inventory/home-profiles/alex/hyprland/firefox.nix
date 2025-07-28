986c0c2fa97c08c47e86f29363b70db588ee8dfd
adeci <alex.decious@gmail.com>
2025-07-28 14:16:30 -0400

    firefox addons

--- inventory/home-profiles/alex/hyprland/firefox.nix
+++ inventory/home-profiles/alex/hyprland/firefox.nix
@@ -1,5 +1,11 @@
+{ inputs, ... }:
 {
   programs.firefox = {
     enable = true;
+    profiles.alex = {
+      extensions.packages = [
+        inputs.firefox-addons.packages."x86_64-linux".ublock-origin
+      ];
+    };
   };
 }
