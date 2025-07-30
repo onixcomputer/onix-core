ddb4a90612af97514a66b6f8ae614b3d779e1162
brittonr <b@robitzs.ch>
2025-07-30 09:28:06 -0400

    update britton-fw

--- machines/britton-fw/configuration.nix
+++ machines/britton-fw/configuration.nix
@@ -1,24 +1,34 @@
-_: {
+{ inputs, pkgs, ... }:
+let
+  grubWallpaper = pkgs.fetchurl {
+    name = "nixos-grub-wallpaper.jpg";
+    url = "https://raw.githubusercontent.com/adeci/wallpapers/main/nix-grub-2880x1920.jpg";
+    sha256 = "sha256-Xu3KlpNMiZzS2fXYGGx0u0Qch7CoEus6ODwNVL4Bq4U=";
+  };
+  streetWallpaper = pkgs.fetchurl {
+    name = "street-wallpaper.png";
+    url = "https://raw.githubusercontent.com/adeci/wallpapers/main/street-full.jpg";
+    sha256 = "sha256-XlSm8RzGwowJMT/DQBNwfsU4V6QuvP4kvwVm1pzw6SM=";
+  };
+in
+{
+  imports = [
+    inputs.grub2-themes.nixosModules.default
+    inputs.sddm-sugar-candy-nix.nixosModules.default
+  ];
+
   networking = {
     hostName = "britton-fw";
     networkmanager.enable = true;
   };
 
-  # disko.devices.disk.main.device = "/dev/disk/by-id/nvme-WD_BLACK_SN850X_4000GB_240243803277";
-
   time.timeZone = "America/New_York";
 
-  services = {
-    xserver.xkb = {
-      layout = "us";
-      variant = "";
-    };
-
-    openssh.enable = true;
-  };
-
-  system.stateVersion = "24.11";
-
+  environment.systemPackages = with pkgs; [
+    imagemagick # required for grub2-theme
+    claude-code
+    comma
+  ];
   zramSwap = {
     enable = true;
     algorithm = "lz4"; # Fast compression
@@ -39,4 +49,56 @@ _: {
     "vm.overcommit_memory" = 1; # Allow overcommit for compilation
     "vm.page-cluster" = 0; # Optimize for ZRAM
   };
+
+  home-manager.backupFileExtension = "backup";
+
+  boot.loader = {
+    timeout = 1;
+    grub = {
+      timeoutStyle = "menu";
+    };
+    grub2-theme = {
+      enable = true;
+      theme = "stylish";
+      footer = true;
+      customResolution = "2880x1920";
+      splashImage = grubWallpaper;
+    };
+  };
+
+  services = {
+    #custom tokyo night theme
+    displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+      sugarCandyNix = {
+        enable = true;
+        settings = {
+          Background = streetWallpaper;
+          ScreenWidth = 2880;
+          ScreenHeight = 1920;
+          FormPosition = "left";
+          HaveFormBackground = true;
+          PartialBlur = true;
+
+          MainColor = "white";
+          AccentColor = "#668ac4";
+          BackgroundColor = "#1a1b26";
+          OverrideLoginButtonTextColor = "white";
+
+          HeaderText = "";
+          DateFormat = "dddd, MMMM d";
+          HourFormat = "HH:mm";
+
+          ForceLastUser = true;
+          ForceHideCompletePassword = true;
+          ForcePasswordFocus = true;
+        };
+      };
+    };
+
+    fwupd.enable = true; # framework bios/firmware updates
+  };
+
+  system.stateVersion = "25.05";
 }
