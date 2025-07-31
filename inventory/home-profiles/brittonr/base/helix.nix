604ab6556940af4b45fae7686ccd0ae5825a8946
brittonr <b@robitzs.ch>
2025-07-31 11:39:06 -0400

    brittonr config updates

--- inventory/home-profiles/brittonr/base/helix.nix
+++ inventory/home-profiles/brittonr/base/helix.nix
@@ -2,5 +2,23 @@
   programs.helix = {
     enable = true;
     defaultEditor = true;
+    settings = {
+      theme = "everblush";
+      editor = {
+        line-number = "relative";
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+      };
+      keys.normal = {
+        space = {
+          space = "file_picker";
+          w = ":w";
+          q = ":q";
+        };
+      };
+    };
   };
 }
