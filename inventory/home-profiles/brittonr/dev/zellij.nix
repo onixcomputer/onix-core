a19341450341126b348d4e19887404039ecabf7b
brittonr <b@robitzs.ch>
2025-07-31 11:39:06 -0400

    brittonr config updates

--- inventory/home-profiles/brittonr/dev/zellij.nix
+++ inventory/home-profiles/brittonr/dev/zellij.nix
@@ -1,5 +1,23 @@
 {
   programs.zellij = {
     enable = true;
+    enableBashIntegration = true;
+    enableFishIntegration = true;
+    enableZshIntegration = true;
+    attachExistingSession = false;
+    exitShellOnExit = false;
+    settings = {
+      show_startup_tips = false;
+      keybinds = {
+        normal = {
+          "bind \"Alt t\"" = {
+            NewTab = { };
+          };
+          "bind \"Alt x\"" = {
+            CloseTab = { };
+          };
+        };
+      };
+    };
   };
 }
