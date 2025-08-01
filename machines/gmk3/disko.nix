9bce3260ee0ff2a45b15bea80abe1f3e1e75c081
brittonr <b@robitzs.ch>
2025-08-01 14:35:41 -0400

    Set disk schema of machine: gmk3 to single-disk

---
+++ machines/gmk3/disko.nix
@@ -0,0 +1,50 @@
+# ---
+# schema = "single-disk"
+# [placeholders]
+# mainDisk = "/dev/disk/by-id/nvme-TWSC_TSC3AN512E6-F2T60S_TTSMA253SX08789" 
+# ---
+# This file was automatically generated!
+# CHANGING this configuration requires wiping and reinstalling the machine
+{
+
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.grub.enable = true;
+  disko.devices = {
+    disk = {
+      main = {
+        name = "main-3ac1539b591f4c7a97d57b657f04ca8c";
+        device = "/dev/disk/by-id/nvme-TWSC_TSC3AN512E6-F2T60S_TTSMA253SX08789";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            "boot" = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+              priority = 1;
+            };
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
