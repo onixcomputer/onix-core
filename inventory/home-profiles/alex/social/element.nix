bf94bf40b6280a4ede133765e4ba0ac3bd6031e2
adeci <alex.decious@gmail.com>
2025-08-02 20:16:54 -0400

    alex laptop hm profile updates

--- inventory/home-profiles/alex/social/element.nix
+++ inventory/home-profiles/alex/social/element.nix
@@ -2,4 +2,16 @@ _: {
   programs.element-desktop = {
     enable = true;
   };
+
+  xdg.desktopEntries.element-desktop = {
+    name = "Element";
+    exec = "element-desktop --password-store=gnome-libsecret %u";
+    icon = "element";
+    type = "Application";
+    categories = [
+      "Network"
+      "InstantMessaging"
+      "Chat"
+    ];
+  };
 }
