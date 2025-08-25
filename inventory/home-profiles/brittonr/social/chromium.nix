47d8145704a5637e9c02a39af26f1dfc2c622ecf
brittonr <b@robitzs.ch>
2025-08-25 10:50:41 -0400

    add chromium extensions

--- inventory/home-profiles/brittonr/social/chromium.nix
+++ inventory/home-profiles/brittonr/social/chromium.nix
@@ -8,6 +8,15 @@ _: {
 
       # Match Hyprland scaling
       "--force-device-scale-factor=1.5"
+
+      # Enable extensions
+      "--enable-extensions"
+      "--enable-remote-extensions"
+    ];
+    extensions = [
+      { id = "cdglnehniifkbagbbombnjghhcihifij"; } # Kagi Search
+      { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
+      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # uBlock Origin
     ];
   };
 }
