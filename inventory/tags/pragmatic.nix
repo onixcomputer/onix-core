77a163d70527507fbdaaa7395cc147358d0c0610
Farid Zakaria <farid.m.zakaria@gmail.com>
2025-10-13 17:53:07 -0700

    Add support for envfs and nix-ld
    I would like to use vscode-server, however the steps to get it to work
    natively are convoluted as visual studio code downloads nodejs runtimes.
    
    For now, add support for envfs/nix-ld which make it tolerable while
    a more pure solution is searched for.

---
+++ inventory/tags/pragmatic.nix
@@ -0,0 +1,21 @@
+{pkgs, ...}: {
+  services = {
+    # A fuse filesystem that dynamically populates contents of /bin
+    # and /usr/bin/ so that it contains all executables from the PATH
+    # of the requesting process.
+    envfs.enable = true;
+  };
+
+  programs = {
+    # I got tired of facing NixOS issues
+    # Let's be more pragmatic and try to run binaries sometimes
+    # at the cost of sweeping bugs under the rug.
+    nix-ld = {
+      enable = true;
+      libraries = with pkgs; [
+        stdenv.cc.cc.lib
+        zlib # numpy
+      ];
+    };
+  };
+}
