8e69f4ef2b8216a1517f6853aad08f4d764d3ae3
brittonr <b@robitzs.ch>
2026-03-13 17:02:45 -0400

    Enable nix-community cachix in nixConfig
    Propagated to all machines via nix-signing.nix.

--- flake.nix
+++ flake.nix
@@ -1,11 +1,12 @@
 {
   description = "Onix Infrastructure";
 
-  # Uncomment when a binary cache is available:
-  # nixConfig = {
-  #   extra-substituters = [ "https://onix.cachix.org" ];
-  #   extra-trusted-public-keys = [ "onix.cachix.org-1:PLACEHOLDER" ];
-  # };
+  nixConfig = {
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
